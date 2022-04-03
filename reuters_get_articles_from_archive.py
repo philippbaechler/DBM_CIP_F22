ecb034fa5a65d269f3a1c92de2acc34ba41c5b6a
Philipp Bächler <philipp.baechler@gmail.com>
2022-04-03 16:54:45 +0200

    Restart browser after 100 calls to avoid browser crash.

--- reuters_get_articles_from_archive.py
+++ reuters_get_articles_from_archive.py
@@ -5,10 +5,13 @@ import pandas as pd
 import time
 # max page: page=3277
 
+opts = webdriver.ChromeOptions()
+#opts.add_argument('--headless')
+
 
 # %%
 def get_driver():
-    driver = webdriver.Chrome()
+    driver = webdriver.Chrome(options=opts)
     url = 'https://www.reuters.com/news/archive'
     driver.get(url)
     time.sleep(5)
@@ -31,7 +34,11 @@ driver = get_driver()
 
 # %%
 all_links = []
-for idx in range(2000, 2100, 1):
+
+for idx in range(2000, 2010, 1):
+    if idx % 100 == 0:
+        driver.close()
+        driver = get_driver()
     url = f'https://www.reuters.com/news/archive?view=page&page={idx}&pageSize=10'
     all_links.extend(get_articles_from_page(driver, url))
 
@@ -41,9 +48,13 @@ df = pd.DataFrame({"url": all_links})
 df.head()
 
 # %%
-df.shape()
+df.shape
 
 # %%
-df.to_csv("all_links.csv")
+df = df.drop_duplicates().reset_index()
+df.shape
+
+# %%
+df.to_csv("data/all_links.csv")
 
 # %%
