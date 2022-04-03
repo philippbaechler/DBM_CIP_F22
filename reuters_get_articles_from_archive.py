e25008718ab715debfa7b44316e3e22b086dc2e7
Philipp Bächler <philipp.baechler@gmail.com>
2022-04-03 15:43:56 +0200

    Save resulting links in a csv table.

--- reuters_get_articles_from_archive.py
+++ reuters_get_articles_from_archive.py
@@ -1,8 +1,8 @@
 # %%
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-
-
+import pandas as pd
+import time
 # max page: page=3277
 
 
@@ -11,7 +11,7 @@ def get_driver():
     driver = webdriver.Chrome()
     url = 'https://www.reuters.com/news/archive'
     driver.get(url)
-    driver.get(url)
+    time.sleep(5)
     driver.find_element(By.ID, "onetrust-accept-btn-handler").click()
     return driver
 
@@ -19,8 +19,10 @@ def get_driver():
 def get_articles_from_page(driver, url):
     driver.get(url)
     articles = driver.find_elements(By.TAG_NAME, "article")
+    links = []
     for article in articles:
-        print(article.find_element(By.CLASS_NAME, "story-content").find_element(By.TAG_NAME, "a").get_attribute("href"))
+        links.append(article.find_element(By.CLASS_NAME, "story-content").find_element(By.TAG_NAME, "a").get_attribute("href"))
+    return links
 
 
 # %%
@@ -28,9 +30,20 @@ driver = get_driver()
 
 
 # %%
-for idx in range(5):
+all_links = []
+for idx in range(2000, 2100, 1):
     url = f'https://www.reuters.com/news/archive?view=page&page={idx}&pageSize=10'
-    get_articles_from_page(driver, url)
+    all_links.extend(get_articles_from_page(driver, url))
 
+
+# %%
+df = pd.DataFrame({"url": all_links})
+df.head()
+
+# %%
+df.shape()
+
+# %%
+df.to_csv("all_links.csv")
 
 # %%
