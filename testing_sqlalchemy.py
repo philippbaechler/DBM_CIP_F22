f05d736410a80810030e7dc1dad45384282b54a5
Philipp Bächler <philipp.baechler@gmail.com>
2022-04-09 18:45:12 +0200

    Compared three different ways of using the to_sql function and prepare for time analyzing.

--- testing_sqlalchemy.py
+++ testing_sqlalchemy.py
@@ -1,23 +1,71 @@
 # %%
 import pandas as pd
-from sqlalchemy import create_engine
-engine = create_engine('sqlite://', echo=False)
+import sqlalchemy
+import timeit
 
 
-# %%
+# %% load data frame
 keywords = pd.read_csv("data/output/keywords.csv", index_col=0)
-keywords["keyword_id"] = keywords["keyword_id"].astype(int)
 keywords.head()
 
 
-# %%
-df = pd.DataFrame({'name' : ['User 1', 'User 2', 'User 3']})
-df
+# %% constring = "mssql+pyodbc://USERNAME:PASSWORD@DATABASESERVER_IP/DATABASENAME?driver=SQL+Server+Native+Client+11.0"
+usr = "root"
+passwd = ""
+constring = f'mysql+pymysql://{usr}:{passwd}@localhost/'
+dbEngine = sqlalchemy.create_engine(constring, connect_args={'connect_timeout': 10}, echo=False)
+dbEngine.execute("USE mydb")
+
 
 # %%
-df.to_sql('users', con=engine)
+def test_mysql_connection():
+    try:
+        with dbEngine.connect() as con:
+            con.execute("SELECT 1")
+        print('engine is valid')
+    except Exception as e:
+        print(f'Engine invalid: {str(e)}')
 
-# %%
-engine.execute("SELECT * FROM users").fetchall()
+
+def get_content_of_keywords_t():
+    dbEngine.execute("USE mydb")
+    output = dbEngine.execute("SELECT * FROM keywords_t").fetchall()
+    output
+
+
+def delete_content_of_keywords_t():
+    dbEngine.execute("DELETE FROM keywords_t;")
+
+
+# 1. version ".to_sql()"
+def to_sql_version_1():
+    keywords.to_sql(name="keywords_t", con=dbEngine, schema="mydb", if_exists="append", index=False)
+
+
+# 2. version "chunking"
+def to_sql_version_2():
+    keywords.to_sql(name="keywords_t", con=dbEngine, schema="mydb", \
+        if_exists="append", index=False, chunksize=1000)
+
+
+# 3. version  "multi-insert"
+def to_sql_version_3():
+    keywords.to_sql(name="keywords_t", con=dbEngine, schema="mydb", \
+        if_exists="append", index=False, chunksize=1000, method='multi')
+
+
+
+#%%
+print(timeit.timeit(lambda: to_sql_version_1(), number=1))
+delete_content_of_keywords_t()
+
+#%%
+print(timeit.timeit(lambda: to_sql_version_2(), number=1))
+delete_content_of_keywords_t()
+
+#%%
+print(timeit.timeit(lambda: to_sql_version_3(), number=1))
+delete_content_of_keywords_t()
+
 
 # %%
