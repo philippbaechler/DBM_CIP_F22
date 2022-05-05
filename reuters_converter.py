600b0cd32840cf95d99a412d4fe17af8e506f12a
Philipp Bächler <philipp.baechler@gmail.com>
2022-05-05 15:06:51 +0200

    Refactoring: use BeautifulSoup instead of plain text search

--- reuters_converter.py
+++ reuters_converter.py
@@ -3,119 +3,123 @@ import os
 import re
 import pandas as pd
 import glob
+from bs4 import BeautifulSoup as bs
 from helper_converter import *
 
 
 #%%
 curren_dir = os.path.abspath(os.getcwd())
-path_to_data = "data/raw/reuters/2020/1"
+path_to_data = "data/raw/reuters/"
 articles_html = [f for f in glob.glob(path_to_data + "**/**/*.html", recursive=True)]
 print(len(articles_html))
 
 
 #%%
-def get_key_words(html_file):
-    search_string = 'name="keywords" content="'
-    start_idx = html_file.find(search_string) + len(search_string)
-    end_idx = html_file.find('" />', start_idx)
-    return html_file[start_idx:end_idx].split(",")
+def get_key_words(article_soup):
+    try:
+        return article_soup.find("meta", attrs={'name':'keywords'}).attrs["content"].split(",")
+    except:
+        return []
 
 
-def get_title(html_file):
-    search_string = 'name="analyticsAttributes.title" content="'
-    start_idx = html_file.find(search_string) + len(search_string)
-    end_idx = html_file.find('" />', start_idx)
-    return html_file[start_idx:end_idx]
+def get_title(article_soup):
+    try:
+        return article_soup.find("meta", attrs={'name':'analyticsAttributes.title'}).attrs["content"]
+    except:
+        return ""
+    
+
+def get_article_date_time(article_soup):
+    try:
+        return pd.to_datetime(article_soup.find("meta", attrs={'name':'analyticsAttributes.articleDate'}).attrs["content"])
+    except:
+        return ""
 
 
-def get_article_date_time(html_file):
-    search_string = 'name="analyticsAttributes.articleDate" content="'
-    start_idx = html_file.find(search_string) + len(search_string)
-    end_idx = html_file.find('" />', start_idx)
-    return pd.to_datetime(html_file[start_idx:end_idx])
+def get_main_authors(article_soup):
+    try:
+        return article_soup.find("meta", attrs={'name':'analyticsAttributes.author'}).attrs["content"].split(",")
+    except:
+        return ""
 
 
-def get_main_author(html_file):
-    search_string = 'name="Author" content="'
-    start_idx = html_file.find(search_string) + len(search_string)
-    end_idx = html_file.find('">', start_idx)
-    return html_file[start_idx:end_idx]
+def get_reporters_writers_and_editors(article_soup):
+    try:
+        substrings = article_soup.find("div", class_='Attribution-attribution-Y5JpY').find("p").get_text().split(";")
+        reporters = []
+        editors = []
+        writers = []
+        for substring in substrings:
+            reporter = re.split('[eE]diting by', substring)[0]
+            reporter = re.findall(r"[rR]eporting\sby\s(.*)", reporter)
+            if len(reporter) != 0:
+                reporters.append(re.split(' and | & |, ', reporter[0]))
+            editor = re.findall(r"[eE]diting\sby\s(.*)", substring)
+            if len(editor) != 0:
+                editors.append(re.split(' and | & |, ', editor[0]))
+            writer = re.split('[eE]diting by', substring)[0]
+            writer = re.findall(r"[wW]riting\sby\s(.*)", writer)
+            if len(writer) != 0:
+                writers.append(re.split(' and | & |, ', writer[0]))
+        return sum(reporters, []), sum(writers, []), sum(editors, [])
+    except:
+        return [],[],[]
 
 
-def get_reporters_writers_and_editors(html_file):
-    search_string = '<p class="Attribution_content">'
-    start_idx = html_file.find(search_string) + len(search_string)
-    end_idx = html_file.find('</p>', start_idx)
-    substrings = html_file[start_idx:end_idx].split(";")
-    reporters = []
-    editors = []
-    writers = []
-    for substring in substrings:
-        reporter = re.split('[eE]diting by', substring)[0]
-        reporter = re.findall(r"[rR]eporting\sby\s(.*)", reporter)
-        if len(reporter) != 0:
-            reporters.append(re.split(' and | & |, ', reporter[0]))
-        editor = re.findall(r"[eE]diting\sby\s(.*)", substring)
-        if len(editor) != 0:
-            editors.append(re.split(' and | & |, ', editor[0]))
-        writer = re.split('[eE]diting by', substring)[0]
-        writer = re.findall(r"[wW]riting\sby\s(.*)", writer)
-        if len(writer) != 0:
-            writers.append(re.split(' and | & |, ', writer[0]))
-    return sum(reporters, []), sum(writers, []), sum(editors, [])
+def get_url(article_soup):
+    try:
+        return article_soup.find("meta", attrs={'name':'analyticsAttributes.canonicalUrl'}).attrs["content"]
+    except:
+        return ""
 
 
-def get_url(html_file):
-    search_string = 'name="analyticsAttributes.canonicalUrl" content="'
-    start_idx = html_file.find(search_string) + len(search_string)
-    end_idx = html_file.find('" />', start_idx)
-    return html_file[start_idx:end_idx]
+def get_contentChannel(article_soup):
+    try:
+        return article_soup.find("meta", attrs={'name':'analyticsAttributes.topicChannel'}).attrs["content"]
+    except:
+        return ""
 
 
-def get_contentChannel(html_file):
-    search_string = 'name="analyticsAttributes.topicChannel" content="'
-    start_idx = html_file.find(search_string) + len(search_string)
-    end_idx = html_file.find('" />', start_idx)
-    return html_file[start_idx:end_idx]
+def get_description(article_soup):
+    try:
+        return article_soup.find("meta", attrs={'name':'description'}).attrs["content"]
+    except:
+        return ""
 
 
-def get_description(html_file):
-    search_string = 'name="description" content=\''
-    start_idx = html_file.find(search_string) + len(search_string)
-    end_idx = html_file.find('\' />', start_idx)
-    return html_file[start_idx:end_idx]
-
-
-def get_paragraphs(html_file):
-    paragraphs = re.findall(r"<p>(.*?)</p>", html_file)
+def get_paragraphs(article_soup):
+    paragraphs = article_soup.find_all("p", class_="Paragraph-paragraph-2Bgue ArticleBody-para-TD_9x")
+    paragraphs = [paragraph.get_text() for paragraph in paragraphs]
     paragraphs = [remove_html_content(paragraph) for paragraph in paragraphs]
+    paragraphs = [paragraph.replace("\n         ", "") for paragraph in paragraphs]
+    paragraphs = [paragraph.replace("\n        ", "") for paragraph in paragraphs]
     return paragraphs
 
 
-def get_article_id(html_file):
-    search_string = 'data-scrolltoid="'
-    start_idx = html_file.find(search_string) + len(search_string)
-    end_idx = html_file.find('"', start_idx)
-    return html_file[start_idx:end_idx]
+def get_article_id(article_url):
+    return article_url.split("-id")[-1]
 
 
 # %%
 articles = []
 
-for article in articles_html:
+for idx, article in enumerate(articles_html):
+    if idx >= 10:
+        break
     with open(article) as data:
-        text_raw = data.read()
-        text_raw = fix_unknown_characters(text_raw)
-        key_words = get_key_words(text_raw)
-        title = get_title(text_raw)
-        article_date_time = get_article_date_time(text_raw)
-        main_author = get_main_author(text_raw)
-        reporters, writers, editors = get_reporters_writers_and_editors(text_raw)
-        article_url = get_url(text_raw)
-        content_channel = get_contentChannel(text_raw)
-        description = get_description(text_raw)
-        paragraphs = join_paragraphs_to_text(get_paragraphs(text_raw))
-        article_id = get_article_id(text_raw)
+        data = data.read()
+        data = fix_unknown_characters(data)
+        soup = bs(data, 'html.parser')
+        key_words = get_key_words(soup)
+        title = get_title(soup)
+        article_date_time = get_article_date_time(soup)
+        main_author = get_main_authors(soup)
+        reporters, writers, editors = get_reporters_writers_and_editors(soup)
+        article_url = get_url(soup)
+        content_channel = get_contentChannel(soup)
+        description = get_description(soup)
+        paragraphs = join_paragraphs_to_text(get_paragraphs(soup))
+        article_id = get_article_id(article_url)
         articles.append({"article_date_time": article_date_time, "title": title, "description": description, \
                          "article_id": article_id, "main_author": main_author, "reporters": reporters, \
                          "writers": writers, "editors": editors, "content_channel": content_channel, \
