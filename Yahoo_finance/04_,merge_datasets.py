4f2ca1f22aa760bdc4ec12b3029c57f615fdb2c4
lisngr <l.isngr@gmail.com>
2022-05-26 23:37:29 +0200

    Updated comments

--- Yahoo_finance/04_,merge_datasets.py
+++ Yahoo_finance/04_,merge_datasets.py
@@ -23,7 +23,6 @@ pd.set_option('expand_frame_repr', False)
 
 # %%
 # Loading of dataset. Load empty dataset if File not found
-file = 'Yahoo_BTC_src_dirty.csv'
 
 # %% load data
 df_bitcoin = pd.read_csv("../data/output/clean/Yahoo_BTC_src_stage.csv", index_col='Date',
@@ -36,45 +35,48 @@ df_kyodo_articles = pd.read_csv("../data/output/normalized_stage/Kyodo_articles_
 df_reuters_articles = pd.read_csv("../data/output/normalized_stage/articles.csv", index_col=0,
                                   parse_dates=True,
                                   infer_datetime_format=True)
+# reuters data is in normalized form, we wanna knwo the keywords, so the articels-keywords realtions table as well as
+# he keywords table a also loaded
 df_reuters_article_keyword = pd.read_csv("../data/output/normalized_stage/article_keyword.csv", index_col=0)
 df_reuters_keyword = pd.read_csv("../data/output/normalized_stage/keywords.csv", index_col=0)
 
-
-df_kyodo_articles["Date"] = pd.to_datetime(df_kyodo_articles['datetime']).dt.normalize() # convert Date column items to datetime.date
+# Kyodo news
+# Normalize Date format so only Date without time is kept. Then create newIndex 'Date' and drop old 'datetime' column
+df_kyodo_articles["Date"] = pd.to_datetime(df_kyodo_articles['datetime']).dt.normalize()
 df_kyodo_articles.set_index('Date', inplace=True) # set Date as row index
 df_kyodo_articles.drop('datetime', axis=1, inplace=True)
 df_kyodo_articles.sort_index(inplace=True)
 
+
+# Reuters
 df_reuters = df_reuters_articles.merge(df_reuters_article_keyword, how='left', left_on='id', right_on='article_id')
 df_reuters = df_reuters.merge(df_reuters_keyword, how='left', left_on='keyword_id', right_on='id')
-
-df_reuters_articles["Date"] = pd.to_datetime(df_reuters_articles['datetime']).dt.tz_localize(None) # convert Date column items to datetime.date
+# Normalize Date format so only Date without time is kept. Then create newIndex 'Date' and drop old 'datetime' column
+df_reuters_articles["Date"] = pd.to_datetime(df_reuters_articles['datetime']).dt.tz_localize(None)
+df_reuters_articles["Date"] = df_reuters_articles["Date"].dt.normalize()
 df_reuters_articles.set_index('Date', inplace=True) # set Date as row index
 df_reuters_articles.drop('datetime', axis=1, inplace=True)
 df_reuters_articles.sort_index(inplace=True)
 
-
-
+# info on datasets
 print(df_bitcoin.info())
 print(df_kyodo_articles.info())
 print(df_reuters_articles.info())
 
 
-
 # merge three data sets
-
 # compile the list of dataframes to merge
 data_frames = [df_bitcoin, df_kyodo_articles, df_reuters_articles]
 
 
 df_merged = reduce(lambda left, right: pd.merge(left, right, on=['Date'],
-                                                how='outer'), data_frames)
+                                                how='inner'), data_frames)
 
-df_merged.to_csv('../data/output/merged/merged.csv',  na_rep='.')
-
-
-df_bitcoin_keywords = df_merged.query('keywords.str.contains("abc")', engine='python')
-print(df_bitcoin_keywords)
-
-
-# %% Clean Data
+file = 'merged.csv'
+outdir = '../data/output/merged'
+if not os.path.exists(outdir):
+    os.makedirs(outdir)
+try:
+    df_merged.to_csv(os.path.join(outdir, file), na_rep='.')
+except:
+    print(f'could not store data')
